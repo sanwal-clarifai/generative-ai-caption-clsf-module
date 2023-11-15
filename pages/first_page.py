a3669e28a92ad35ee945680c7c21339195ff67b4
sanwal-clarifai <60618046+sanwal-clarifai@users.noreply.github.com>
2023-11-15 18:04:43 -0500

    fixed the code and it is good to go now

--- pages/first_page.py
+++ pages/first_page.py
@@ -16,39 +16,32 @@ import base64
 from PIL import Image
 import io
 
-
-# channel = ClarifaiChannel.get_json_channel()
-# stub = service_pb2_grpc.V2Stub(channel)
-
-# channel_prod = ClarifaiChannel.get_grpc_channel()
-# stub_prod = service_pb2_grpc.V2Stub(channel_prod)
-
 auth = ClarifaiAuthHelper.from_streamlit(st)
+print(auth._pat)
 stub = auth.get_stub()
 userDataObject = auth.get_user_app_id_proto()
+userDataClarifaiMain= resources_pb2.UserAppIDSet(user_id='clarifai', app_id='main')
+print(userDataObject)
 
-def model_predict_by_file(auth_obj,
-                          image_path,
+def model_predict_by_file(image_bytes,
                           MODEL_ID):
-    with open(image_path, "rb") as f:
-        file_bytes = f.read()
-
-    post_model_outputs_response = stub_prod.PostModelOutputs(
+    
+    post_model_outputs_response = stub.PostModelOutputs(
         service_pb2.PostModelOutputsRequest(
-            user_app_id=userDataObjectProd,
+            user_app_id=userDataClarifaiMain,
             # The userDataObject is created in the overview and is required when using a PAT
             model_id=MODEL_ID,
             inputs=[
                 resources_pb2.Input(
                     data=resources_pb2.Data(
                         image=resources_pb2.Image(
-                            base64=file_bytes
+                            base64=image_bytes
                         )
                     )
                 )
             ]
         ),
-        metadata=auth_obj
+        metadata=(('authorization', 'Key ' + auth._pat),)
     )
     return post_model_outputs_response
 
@@ -74,8 +67,7 @@ def model_text_predict(auth_obj,
     )
     return post_model_outputs_response
 
-def workflow_predict(auth_obj,
-                     WORKFLOW_ID,
+def workflow_predict(WORKFLOW_ID,
                      INPUT_TEXT):  # The userDataObject is required when using a PAT
 
     post_workflow_results_response = stub.PostWorkflowResults(
@@ -92,45 +84,21 @@ def workflow_predict(auth_obj,
                 )
             ]
         ),
-        metadata=auth_obj
+        metadata = (('authorization', 'Key ' + auth._pat),)
+
     )
     return post_workflow_results_response
 
 
-if __name__ == '__main__':
-    pat = "54febaf57e214497ad5bb380b45abc73"  # DEV PAT
-    pat_prod = "a55a5b7c56df4ef6a111d7fbd5f5d059"  # PROD PAT
+if __name__ == '__main__':    
 
-    api_auth = (('authorization', 'Key ' + pat),)
-    api_auth_prod = (('authorization', 'Key ' + pat_prod),)
-
-    APP_ID = "sd-gai-text"
-    USER_ID = "sanwalyousaf-dev"
-    # APP_ID = "stable-diffusion"
-    # USER_ID = "pv9"
-
-    APP_ID_PROD = "pepsi-demo-app"
-    USER_ID_PROD = "sanwalyousaf"
-
-    userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID,
-                                                app_id=APP_ID)
-    userDataObjectProd = resources_pb2.UserAppIDSet(user_id=USER_ID_PROD,
-                                                    app_id=APP_ID_PROD)
-
-    ####Make predictions to the Stable  Diffusion Workflow in Dev
-
-    workflow_id = "sd-text-clsf"
-
-    clarifai_logo = "Clarifai_Logo_FC_Logo.jpg"
-    image = Image.open(clarifai_logo)
+    # clarifai_logo = "pages/Clarifai_Logo_FC_Logo.jpg"
+    # image = Image.open(clarifai_logo)
     # st.image(image, use_column_width=True, caption="Clarifai Logo", width=100)
 
     st.title("Generative AI and Clarifai - Stable Diffusion")
-    print(auth)
-    st.stop()
-
     with st.sidebar:
-        st.image(image, use_column_width=True, caption="Clarifai Logo", width=100)
+        # st.image(image, use_column_width=True, caption="Clarifai Logo", width=100)
         text = st.text_input("Input the text to generate the image from")
         model_options = st.multiselect("Select the models to run the generated image through",
                                        ["general-english-image-caption-clip",
@@ -146,28 +114,28 @@ if __name__ == '__main__':
 
     if inp_button:
         with st.spinner("Generating Image..."):
-            workflow_payload = workflow_predict(api_auth, workflow_id, text)
-
-            image_b64 = workflow_payload.results[0].outputs[0].data.image.base64
-            # b64_decode = base64.b64decode(image_b64)
+            workflow_payload = workflow_predict( WORKFLOW_ID="stable-diffusion-xl",
+                                                 INPUT_TEXT=text)
+            image_b64 = workflow_payload.results[0].outputs[0].data.image.base64   
             image = Image.open(io.BytesIO(image_b64))
-            image.save("generated_image.jpg")
+            # image.save("generated_image.jpg")
             st.image(image, use_column_width=True, caption="Generated Image", width=100)
             st.success("Image Generated Successfully")
 
         # With image saved now run it through models in prod
+
         for i in range(len(model_options)):
             with st.spinner(f"predicting to model {model_options[i]}"):
                 if model_options[i] == "general-english-image-caption-clip":
                     model_id = "general-english-image-caption-clip"
-                    model_payload = model_predict_by_file(api_auth_prod, "generated_image.jpg", model_id)
+                    model_payload = model_predict_by_file(image_bytes=image_b64, MODEL_ID=model_id)
                     st.write(f"**Model: {model_options[i]}**")
                     st.write(f"**Image Caption:**")
                     st.write(model_payload.outputs[0].data.text.raw)
                 elif model_options[i] == "general-image-recognition":
                     df = pd.DataFrame(columns=["Concept", "Probability"])
                     model_id = "general-image-recognition"
-                    model_payload = model_predict_by_file(api_auth_prod, "generated_image.jpg", model_id)
+                    model_payload = model_predict_by_file(image_bytes=image_b64, MODEL_ID=model_id)
                     st.write(f"**Model: {model_options[i]}**")
                     st.write(f"**Image Tags:**")
 
